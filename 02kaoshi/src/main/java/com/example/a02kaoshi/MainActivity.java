e5dcf80fa4ef970aebf2f3d21a43e7f469f4ccc5
lwt <727141749@qq.com>
2017-10-31 10:42:12 +0800

    123

---
+++ 02kaoshi/src/main/java/com/example/a02kaoshi/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.a02kaoshi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mAccount;                        //用户名编辑
+    private EditText mPwd;                            //密码编辑
+    private EditText mPwdCheck;                       //密码编辑
+    private Button mSureButton;                       //确定按钮
+    private Button mCancelButton;                     //取消按钮
+    private UserDataManager mUserDataManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAccount = (EditText) findViewById(R.id.resetpwd_edit_name);
+        mPwd = (EditText) findViewById(R.id.resetpwd_edit_pwd_old);
+
+        mSureButton = (Button) findViewById(R.id.register_btn_sure);
+
+        mSureButton.setOnClickListener(m_register_Listener);      //注册界面两个按钮的监听事件
+        mCancelButton.setOnClickListener(m_register_Listener);
+
+        if (mUserDataManager == null) {
+            mUserDataManager = new UserDataManager(this);
+            mUserDataManager.openDataBase();                              //建立本地数据库
+        }
+    }
+    View.OnClickListener m_register_Listener = new View.OnClickListener() {    //不同按钮按下的监听事件选择
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.register_btn_sure:                       //确认按钮的监听事件
+                    register_check();
+                    break;
+
+            }
+        }
+    };
+    public void register_check() {                                //确认按钮的监听事件
+        if (isUserNameAndPwdValid()) {
+            String userName = mAccount.getText().toString().trim();
+            String userPwd = mPwd.getText().toString().trim();
+            String userPwdCheck = mPwdCheck.getText().toString().trim();
+            //检查用户是否存在
+            int count=mUserDataManager.findUserByName(userName);
+            //用户已经存在时返回，给出提示文字
+            if(count>0){
+                return ;
+            }
+
+        }
+    }
+    public boolean isUserNameAndPwdValid() {
+        if (mAccount.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.app_name),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+}
+
